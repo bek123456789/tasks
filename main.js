236552643338e011e426780555a88bb4e6e04c69
bek123456789 <redmirating@gmail.com>
2024-05-31 15:24:07 +0500

    done

---
+++ main.js
@@ -0,0 +1,126 @@
+
+// 1-zd
+
+// let number = +prompt('Biror bir son yozing')
+
+// function divisible (number){
+//     return number % 3 === 0 && number % 5 === 0;
+// }
+
+// alert(divisible(number));
+
+
+
+
+
+// 2-zd
+
+// let number = prompt('Biror bir son kiriting!');
+// let oddNumber = '';
+// let evenNumber = '';
+// let i = 0;
+
+// while (number[i]) {
+//     if (number[i] % 2 === 0) {
+//         evenNumber = number[i];
+//     } else {
+//         oddNumber = number[i];
+//     }
+//     i++;
+// }
+
+// evenNumber && alert('Even numbers: ' + evenNumber);
+// oddNumber && alert('Odd numbers: ' + oddNumber);
+
+
+
+// 3-zd
+
+
+
+// let arr = [1, 6, 4, 9, 2, 7]
+// arr.sort((a, b) => a - b);
+// console.log(arr);
+
+
+
+
+
+
+// 5- zd
+
+// let obj1 = { name: 'Bekzod', age: 13 };
+// let obj2 = { name: 'Bekzod', age: 13,  };
+
+// function checkObj(obj1, obj2) {
+
+//     let trueObj = false;
+
+//     for (let key in obj1) {
+//         if ((key in obj2)) {
+//             trueObj = true;
+//         } else {
+//             trueObj = false;
+//         }
+//     }
+
+//     for (let key in obj2) {
+//         if ((key in obj1)) {
+//             trueObj = true;
+//         } else {
+//             trueObj = false;
+//         }
+//     }
+
+//     return trueObj;
+// }
+
+// console.log(checkObj(obj1, obj2)); 
+
+
+
+
+// 6-zd Fizz Buzz
+
+
+
+
+// for (let i = 1; i < 100; i++) {
+//     if (i % 15 == 0) {
+//         console.log('Fizzbuzz');
+//     }
+//     else if (i % 3 == 0) {
+//         console.log('Fizz');
+//     }
+//     else if (i % 5 == 0) {
+//         console.log('Buzz');
+
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+
+
+// 2...2 zd  
+
+// let arr = [[2], 23, 'dance', true, [3, 5, 3], [65, 45]]
+
+// let filterArr = arr.filter(element => Array.isArray(element))
+
+// let flatArr = filterArr.flat();
+
+
+
+
+
+// let sortArr = flatArr.sort((a, b) => a - b)
+// console.log(sortArr);
+
+
+
+
+
+
+
